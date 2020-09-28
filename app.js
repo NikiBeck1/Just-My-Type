79cd9eaa1d6aef450ca427e0e3eb75ea69918018
Niki Beck <nikibeck@Nikis-iMac-Pro.attlocal.net>
2020-09-28 14:56:15 -0500

    Fixed the spacing tto make sure the senctences read correctly and move on to the next one correctly.

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ $(function () {
     
     // Variables
     let sentences = [
-        'ten',
-        'ate neite ate nee enet ite ate inet ent eate', 
-        'Too ato too nOt enot one totA not anot tOO aNot', 
-        'oat itain oat tain nate eate tea anne inant nean', 
-        'itant eate anot eat nato inate eat anot tain eat', 
-        'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+        'ten ',
+        'ate neite ate nee enet ite ate inet ent eate ', 
+        'Too ato too nOt enot one totA not anot tOO aNot ', 
+        'oat itain oat tain nate eate tea anne inant nean ', 
+        'itant eate anot eat nato inate eat anot tain eat ', 
+        'nee ene ate ite tent tiet ent ine ene ete ene ate '];
 
     let i = 0;
     let j = 0;
